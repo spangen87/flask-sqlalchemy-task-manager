7f3b94f3cd520d76a4943d0be105174be150b2ff
spangen87 <rikard.spangmyr@bestlas.se>
2022-09-19 13:38:43 +0000

    Add function to add tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initalization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker init
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    });
+
+    // select intialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
